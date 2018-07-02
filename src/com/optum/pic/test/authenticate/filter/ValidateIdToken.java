3dbad49dc97decd13b6b4f06470502e81e858961
Chintalapudi, Dheeraj <ChintalD@advisory.com>
2018-07-02 10:26:24 -0500

    Adding the necessary Jars and commiting the code

--- src/com/optum/pic/test/authenticate/filter/ValidateIdToken.java
+++ src/com/optum/pic/test/authenticate/filter/ValidateIdToken.java
@@ -3,13 +3,27 @@ package com.optum.pic.test.authenticate.filter;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.Validate;
+
+import org.jose4j.base64url.Base64Url;
+import org.jose4j.jws.JsonWebSignature;
+import org.jose4j.jwx.CompactSerializer;
+import org.jose4j.keys.HmacKey;
+import org.jose4j.lang.StringUtil;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
+
 
 public class ValidateIdToken {
 	private final List mClientIDs;
@@ -46,7 +60,47 @@ public class ValidateIdToken {
         }
         return payload;
     }
+    
+    private void verifyJwtSignatureAndUpdateResult(Map<String, String> result, String id_token, String secret) {
+    	String decoded_header = null;
+    	String decoded_payload = null;
+    	String sign_verification_status = null;
 
+
+    	try {
+    		String[] parts = CompactSerializer.deserialize(id_token);
+    		Validate.isTrue(parts.length == 3);
+    		Base64Url base64url = new Base64Url();
+    		decoded_header = base64url.base64UrlDecodeToString(parts[0], StringUtil.UTF_8);
+    		decoded_payload = base64url.base64UrlDecodeToString(parts[1], StringUtil.UTF_8);
+    		Map<String, Object> header = mapJson(decoded_header);
+    		String alg = (String) header.get("alg");
+    		Validate.isTrue(alg != null && alg.startsWith("HS"), "Unsupported algorithm");
+    		JsonWebSignature jws = new JsonWebSignature();
+    		jws.setCompactSerialization(id_token);
+    		byte[] bytes = secret.getBytes("UTF-8");
+    		HmacKey publicKey = new HmacKey(bytes);
+    		jws.setKey(publicKey);
+    		sign_verification_status = jws.verifySignature() ? "success": "error";
+    		} catch (Exception ex) {
+			//logger.error("Error in validating id_token signature", ex);
+			System.out.println("Error in validating id_token signature: "+ex);
+			sign_verification_status = "error";
+		}
+    	result.put("decoded_header", decoded_header);
+    	result.put("decoded_payload", decoded_payload);
+    	result.put("sign_verification_status", StringUtils.capitalize(sign_verification_status));
+       }
+    
+	private Map<String, Object> mapJson(String json) {
+	    try {
+	    	TypeReference<HashMap<String, Object>> typeRef = new TypeReference<HashMap<String, Object>>() {};
+	    	return mapper.readValue(json, typeRef);
+	    	} catch (Exception ex) {
+	    		throw new RuntimeException(ex);    
+	    	}
+	}
+	private ObjectMapper mapper = new ObjectMapper();
     public String problem() {
         return mProblem;
     }
